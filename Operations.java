f21f70670123f91d3d20721146c45837d295876b
adeeljameel810-byte <adeeljameel810@gmail.com>
2026-04-08 11:06:05 +0500

    Simple Calculation Operations

---
+++ Operations.java
@@ -0,0 +1,16 @@
+public class Operations {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 10;
+        
+        System.out.println("The sum of num1 and num 2 is: " +(num1+num2));
+        System.out.println("The subtraction of num1 and num2 is: " +(num1-num2));
+        System.out.println("The multiplication of num1 and num2 is; " +(num1*num2));
+        System.out.println("The division of num1 and num2 is: "+ (num1/num2));
+        System.out.println("The modulus of num1 and num2 is : " +(num1%num2));
+    
+
+
+        
+    }
+}
