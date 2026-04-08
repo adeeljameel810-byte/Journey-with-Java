4b90f3070985decd3559ee0706f0483d220bd26a
adeeljameel810-byte <adeeljameel810@gmail.com>
2026-04-08 10:07:38 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+ public class Main {
+    public static void main(String[] args) {
+        System.out.print(
+            "**************************************************\n" +
+            "Welcome to the software engineering department\n" +
+            "My name is jameel Ahmed having student Id K25Sw009\n" +
+            "Address: Department of Software engineering\n" +
+            "Mehran University of engineering and technology\n" +
+            "Shaheed Zulfqar Ali bhutto Campus"
+        );
+
+        
+    }
+ }
+    
+
