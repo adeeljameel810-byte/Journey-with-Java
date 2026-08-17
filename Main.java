dafb7704ceb3de63c2c4d904388b47722128d996
Jameel Ahmed <adeeljameel810@gmail.com>
2026-08-18 00:41:15 +0500

    Fix capitalization in welcome message
    Corrected the capitalization of names and terms in the output message.

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@
         System.out.print(
             "**************************************************\n" +
             "Welcome to the Software Engineering Department\n" +
-            "My name is jameel Ahmed having student Id K25Sw009\n" +
-            "Address: Department of Software engineering\n" +
-            "Mehran University of engineering and technology\n" +
+            "My name is Jameel Ahmed having student Id K25SW009\n" +
+            "Address: Department of Software Engineering\n" +
+            "Mehran University of Engineering and Technology\n" +
             "Shaheed Zulfqar Ali bhutto Campus Khairpur Mir's"
         );
 
