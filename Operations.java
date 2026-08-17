2c1a488e7856fb5ba30de22db8964648aee69e52
Jameel Ahmed <adeeljameel810@gmail.com>
2026-08-18 00:35:15 +0500

    Fix formatting and spacing in Operations.java - improve code consistency and readability

--- Operations.java
+++ Operations.java
@@ -3,14 +3,10 @@ public class Operations {
         int num1 = 20;
         int num2 = 10;
         
-        System.out.println("The sum of num1 and num 2 is: " +(num1+num2));
-        System.out.println("The subtraction of num1 and num2 is: " +(num1-num2));
-        System.out.println("The multiplication of num1 and num2 is; " +(num1*num2));
-        System.out.println("The division of num1 and num2 is: "+ (num1/num2));
-        System.out.println("The modulus of num1 and num2 is : " +(num1%num2));
-    
-
-
-        
+        System.out.println("The sum of num1 and num2 is: " + (num1 + num2));
+        System.out.println("The subtraction of num1 and num2 is: " + (num1 - num2));
+        System.out.println("The multiplication of num1 and num2 is: " + (num1 * num2));
+        System.out.println("The division of num1 and num2 is: " + (num1 / num2));
+        System.out.println("The modulus of num1 and num2 is: " + (num1 % num2));
     }
 }
