0f60807ad880006f45ef760f7ad584653a97656e
Jameel Ahmed <adeeljameel810@gmail.com>
2026-08-18 00:38:06 +0500

    Modify welcome message in Main.java
    Updated the welcome message to include the campus location.

--- Main.java
+++ Main.java
@@ -2,11 +2,11 @@
     public static void main(String[] args) {
         System.out.print(
             "**************************************************\n" +
-            "Welcome to the software engineering department\n" +
+            "Welcome to the Software Engineering Department\n" +
             "My name is jameel Ahmed having student Id K25Sw009\n" +
             "Address: Department of Software engineering\n" +
             "Mehran University of engineering and technology\n" +
-            "Shaheed Zulfqar Ali bhutto Campus"
+            "Shaheed Zulfqar Ali bhutto Campus Khairpur Mir's"
         );
 
         
